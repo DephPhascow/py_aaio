0aa04fac8963c0d9486335305e8df9ab242bd446
DephPhascow <d.sinisterpsychologist@gmail.com>
2023-10-22 11:12:12 +0500

    Update

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import setup, find_packages
 readme = ""
 license = ""
 
-with open("README.md", "r") as fh:
+with open("README.MD", "r") as fh:
     readme = fh.read()
 with open("LICENCE", "r") as fh:
     license = fh.read()
  
 setup(
-    name = "",
-    version = "1.1.0",
-    keywords = ("", ),
-    description = "",
+    name = "py_aaio",
+    version = "1.1.3",
+    keywords = ("aaio", ),
+    description = "Simple library for AAIO API",
     long_description = readme,
     license = license,
-    url = "",
-    author = "",
-    author_email = "",
+    url = "https://github.com/DephPhascow/py_aaio",
+    author = "DephPhascow",
+    author_email = "d.sinisterpsychologist@gmail.com",
     packages = find_packages(),
     include_package_data = True,
-    platforms = "",
-    install_requires = [""]
+    platforms = "any",
+    install_requires = ["strenum", "requirements"]
 )
--- tests/main.py
+++ tests/main.py
@@ -1,7 +1,20 @@
+from py_aaio.AAIO import AAIO
+from py_aaio import models
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+AAIO_ID = os.getenv("AAIO_ID")
+AAIO_KEY = os.getenv("AAIO_KEY")
+AAIO_SECRET_1 = os.getenv("AAIO_SECRET_1")
+AAIO_SECRET_2 = os.getenv("AAIO_SECRET_2")
 
 def run():
-    pass
+    aaio = AAIO(AAIO_ID, AAIO_SECRET_1, AAIO_SECRET_2, AAIO_KEY)
+    url = AAIO.get_ips()
+    balance = aaio.get_balance()
+    print(balance.balance)
 
 if __name__ == "__main__":
     run()
